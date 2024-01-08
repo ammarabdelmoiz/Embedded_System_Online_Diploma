7ebbb44046375d80b2aaa47928e377300a332368
Ammar Abdelmoiz <ammar.abdelmoiz44@gmail.com>
2024-01-08 18:38:09 +0200

    Folder c_programming assignment for course c

---
+++ C_Programming/H.W1/EX1.c
@@ -0,0 +1,23 @@
+/*
+ * EX1.c
+ *
+ *  Created on: Jan 6, 2024
+ *      Author: Ammar Abdelmoiz
+ */
+
+
+#include <stdio.h>
+
+int main()
+{
+	printf("##########Console-output### \n C Programming");
+
+}
+
+
+
+
+
+
+
+
---
+++ C_Programming/H.W1/EX2.c
@@ -0,0 +1,19 @@
+/*
+ * EX2.c
+ *
+ *  Created on: Jan 6, 2024
+ *      Author: Ammar Abdelmoiz
+ */
+
+
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	printf("Enter an intger:");
+	fflush(stdin), fflush(stdout);
+	scanf("%d",&x);
+	printf("You entered: %d",x);
+	return 0;
+}
